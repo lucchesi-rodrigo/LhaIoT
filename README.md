7c03ae865ab0caa0a2a7514410e34a088baf328c
Lucchesi <rodrigo.schmidt.lucchesi@gmail.com>
2021-12-17 23:25:19 -0300

    Adding new update to lib

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # LHAOT
 linux-hardware-optimization-tools
+
+The idea is to create a series of optimization services to improve energy consumption on embeded systems.
+
+Take actions based on state of health
+Take actions based on state of charge
+Create smart actions based on reinforcment learning
+
+os collect data -> cloud -> ms -> curls to adapt system
--- energy/soh.sh
+++ energy/soh.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
-state_of_health () {
-    local battery_state=`acpi | awk 'NR==1{print $4}'`
-    local state_of_charge=`acpi | awk 'NR==1{print $5}'`
-    # apply processes
 
+function energy_state {
+
+    local state_of_charge=`acpi | awk 'NR==1{print $5}'`
+    local time_remaning=`acpi | awk 'NR==1{print $6}'
+    local temp_core_1=`sensors | awk 'NR==4{print $4}'
+    local temp_core_2=`sensors | awk 'NR==5{print $4}'`
+    # save to csv	
 
 }
 
-func_result="$(optimize_memory)"
-echo $func_result
+function energy_statistics {
 
+    local state_of_charge=`acpi | awk 'NR==1{print $5}'`
+    local time_remaning=`acpi | awk 'NR==1{print $6}' 
+    local temp_core_1=`sensors | awk 'NR==4{print $4}'
+    local temp_core_2=`sensors | awk 'NR==5{print $4}'` 
+    # save to csv       
 
+}
 
+function eco_mode {
+
+    local state_of_charge=`acpi | awk 'NR==1{print $5}'`
+    local time_remaning=`acpi | awk 'NR==1{print $6}' 
+    local temp_core_1=`sensors | awk 'NR==4{print $4}'
+    local temp_core_2=`sensors | awk 'NR==5{print $4}'` 
+    # save to csv       
+
+}
 
